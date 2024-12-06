aad22f3b25055aca947105d307aee953611b15bf
MonDSer <MonDSer@DESKTOP-U4UDGKL>
2024-12-05 22:10:32 -0500

    Commit del Proyecto en Etapa Avanzada - Versión Final

--- src/main/java/com/techinnovate/techinnovate/controller/service/ProductoService.java
+++ src/main/java/com/techinnovate/techinnovate/controller/service/ProductoService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 /*import java.util.ArrayList;*/
 import java.util.List;
 import com.techinnovate.techinnovate.repository.ProductoRepository;
-import jakarta.transaction.Transactional;
 
 @Service
     public class ProductoService {
